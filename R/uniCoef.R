ac0191167e94c9d53d8dd7f905af3ee8f060bccc
tibshirani <tibs@stanford.edu>
2025-02-23 05:53:08 -0800

    Made changes to doc files
    made small changes to documentation  sections in the .R files

--- R/uniCoef.R
+++ R/uniCoef.R
@@ -1,28 +1,31 @@
 #' Compare the nonzero coefficients and univariate counterparts
 #'
-#' For a `cv.uniLasso` object, compare the CV-selected nonzero coefficints to their univariate counterparts. Also works for a `cv.glmne`t object.
+#' For a `cv.uniLasso` object, compare the CV-selected nonzero coefficients to their univariate counterparts. Also works for a `cv.glmnet` object.
 #'
 #' @param cv.object A `cv.uniLasso` or a `cv.glmnet` object.
 #' @param info The result of a call to `uniInfo()`. If `cv.object` inherits from `cv.uniLasso`, the `$info` component will be used.
-#' @param s the value of lambda to be used, with default `s="lambda.min"`. Alternatively, can be `s="lambda.1se".
+#' @param s The value of lambda to be used, with default `s="lambda.min"`. Alternatively, can be `s="lambda.1se".
 #' @return a two columns matrix with one column being the non-zero coefficients from the `cv.object`, and the other being the corresponding univariate coefficients.
 #'
-#'  @examples
-#'
-#' sigma =3
+#' @examples
+#' 
+#' sigma <-3
 #' set.seed(1)
-#' n <- 100; p <- 20
+#' n <- 100;
+#' p <- 20
 #' x <- matrix(rnorm(n * p), n, p)
 #' beta <- matrix(c(rep(2, 5), rep(0, 15)), ncol = 1)
 #' y <- x %*% beta + rnorm(n)*sigma
 #'
 #' cvfit <- cv.uniLasso(x, y)
 #' uniCoef(cvfit)
-#'
+
 #' cvfit2 <- cv.glmnet(x,y)
 #' uniCoef(cvfit2, info=cvfit$info)
 #'
 #' @export
+
+
 
 uniCoef <- function(cv.object, info=NULL, s=c("lambda.min","lambda.1se"),...){
     type="uniLasso"
